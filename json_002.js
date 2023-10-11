00827f73ac69a35247b2400087b2070d73bff740
YANGJIHUN <ik5334@naver.com>
2023-10-11 22:30:48 +0900

    first commit

---
+++ json_002.js
@@ -0,0 +1,13 @@
+//JSON 데이터를 다루기위한 JS 사용 방법
+
+
+//[1]: 데이터
+const person = [
+    {"name":"홍길동","age":20,"nationality":"대한민국"},
+    {"name":"이순신","age":30,"nationality":"미국"},
+    {"name":"강감찬","age":40,"nationality":"영국"},
+    {"name":"을지문덕","age":50,"nationality":"프랑스"},
+
+];
+
+console.log(typeof person);    
