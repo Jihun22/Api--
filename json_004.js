f0ac3fd2be5ca27ede6c7be3be1f6322a7daef5c
YANGJIHUN <ik5334@naver.com>
2023-10-23 22:02:01 +0900

    json 콘솔 테스트

--- json_004.js
+++ json_004.js
@@ -1,8 +1,8 @@
 // 중첩된 json 데이터 다루기
 //한권의 도서 정보를 갖고 있는 JSON 데이터에 해당 도서가 속해있는 카테고리를 출력하시오.
 
+const book ={
 
-{
     "isbn":"123-456-789",
     "author":{
         "name":"홍길동",
@@ -15,3 +15,6 @@
     "title":"대한민국의 정의는 죽었는가?",
     "category":["Non-Fiction","IT","Politics"]
 }
+
+console.log(book["author"].name);   //홍길동 
+console.log(book["editor"].name);   //이순신
